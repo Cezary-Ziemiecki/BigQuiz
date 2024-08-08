eb5b90e83ccb864a2ebc23557dcabd1fbd889c5c
C_Ziemiecki <cezary.ziemiecki@invenco.pl>
2024-08-08 22:39:31 +0200

    v0.0.0 prepare MongoRecords based classes

---
+++ main.py
@@ -0,0 +1,33 @@
+from quiz.game import Game, User, Question
+from pymongo.mongo_client import MongoClient
+from bson.objectid import ObjectId
+uri = "mongodb+srv://manager:manager2000@bigquiz.5ex4d.mongodb.net/?retryWrites=true&w=majority&appName=BigQuiz"
+myclient = MongoClient(uri)
+mydb = myclient["BIGQUIZ"]
+
+test = Game.load_by_id(mydb, ObjectId('66b289cf1279597ce6a3c889'))
+print(test._id)
+print(test.winner)
+print(list(test.users))
+print(test.questions)
+
+Czarek = User.load_by_id(mydb, ObjectId('66b2880d1279597ce6a3c888'))
+print(Czarek._id)
+print(list(Czarek.games))
+print(Czarek.questions)
+Ania = User.load_by_id(mydb, ObjectId('66b4fe33319b66bb55263c4a'))
+print(Ania._id)
+print(list(Ania.games))
+print(Ania.questions)
+
+One = Question.load_by_id(mydb, ObjectId('66b287a21279597ce6a3c887'))
+
+print(One.question)
+print(One.correct_answer)
+print(One.incorrect_answers)
+
+Two = Question(mydb)
+
+print(Two.question)
+print(Two.correct_answer)
+print(Two.incorrect_answers)
---
+++ quiz/game.py
@@ -0,0 +1,211 @@
+import datetime
+import abc
+import collections.abc as collections
+from bson.objectid import ObjectId
+from pymongo.errors import InvalidName
+from pymongo.database import Database
+
+
+class UnchangedProperty(abc.ABC):
+    def __init__(self, default=None) -> None:
+        self.instances = {}
+        self._default = default
+
+    def __get__(self, instance, parent):
+        if instance is None:
+            return self
+        try:
+            return self.instances[instance]
+        except KeyError:
+            return self._default
+        except Exception as e:
+            print(e)
+
+    def __set__(self, instance, value):
+        if instance in self.instances:
+            raise AttributeError("can't set attribute")
+        else:
+            try:
+                value = self.validate(value)
+            except Exception as e:
+                print(e)
+            else:
+                self.instances[instance] = value
+
+    @abc.abstractmethod
+    def validate(self, value):
+        """"""
+
+
+class UnchangedTypedPropert(UnchangedProperty):
+
+    def __init__(self, cls, default=None) -> None:
+        super().__init__(default)
+        self._cls = cls
+
+    def validate(self, value):
+        if isinstance(value, self._cls):
+            return value
+        else:
+            raise TypeError
+
+
+class IterableStorage(abc.ABC):
+
+    def __init__(self):
+        self.storage = []
+
+    def __set__(self, instance, value):
+        raise AttributeError("can't set attribute")
+
+    def __setitem__(self, key, value):
+        try:
+            value = self.validate(value)
+        except Exception as e:
+            print(e)
+        else:
+            self.storage[key] = value
+
+    def __iter__(self):
+        return iter(self.storage)
+
+    def __getitem__(self, index):
+        return self.storage[index]
+
+    def append(self, value):
+        try:
+            value = self.validate(value)
+        except Exception as e:
+            print(e)
+        else:
+            print(self.storage, value)
+            self.storage.append(value)
+
+    @abc.abstractmethod
+    def validate(self, value):
+        """"""
+
+
+class MongoId():
+    def __init__(self, collection):
+        super().__init__()
+        self.collection = collection
+
+    def validate(self, value):
+        print(f'waliduje: {value}')
+        if type(value) == ObjectId:
+            result = self.collection.find({'_id': value})
+        elif type(value) in [str, int]:
+            object_id = ['0']*(24-len(str(value)))+list(str(value))
+            value = ObjectId(''.join(object_id))
+            result = self.collection.find(
+                {'_id': ObjectId(''.join(object_id))})
+        else:
+            raise ValueError('value must be in type ObjectId, str or int')
+        if len(list(result)):
+            if not isinstance(self, collections.Iterable) or value not in list(self):
+                return value
+            raise ValueError(f'Value {value} is in collection')
+        raise InvalidName(f'There was no record with id: {value}')
+
+
+class MongoIdList(MongoId, IterableStorage):
+    """"""
+
+
+class Game:
+    _id = UnchangedTypedPropert(ObjectId)
+    db = UnchangedTypedPropert(Database)
+    winner = UnchangedTypedPropert(ObjectId)
+    game_time = UnchangedTypedPropert(datetime.time)
+
+    def __init__(self, db, games_col='games', users_col='users', guestions_col='questions') -> None:
+        self.db = db
+        self.users = MongoIdList(self.db[users_col])
+        self.questions = []
+        self.is_finished = False
+        self.successes = []
+
+    @classmethod
+    def load_by_id(cls, db, _id, games_col='games', users_col='users', guestions_col='questions'):
+        instance = cls(db, games_col, users_col, guestions_col)
+        instance._id = _id
+        instance.load_from_db(games_col)
+        return instance
+
+    def load_from_db(self, col):
+        try:
+            result = self.db[col].find({'_id': self._id})[0]
+        except Exception as e:
+            raise e
+        else:
+            self.is_finished = result['is_finished']
+            for user in result['users']:
+                self.users.append(user)
+            if result['winner'] != self.winner:
+                self.winner = result['winner']
+            self.questions = result['questions']
+            self.successes = result['successes']
+
+
+class User:
+    _id = UnchangedTypedPropert(ObjectId)
+    db = UnchangedTypedPropert(Database)
+
+    def __init__(self, db, games_col='games', users_col='users', guestions_col='questions') -> None:
+        self.db = db
+        self.games = MongoIdList(self.db[games_col])
+        self.questions = []
+        self.correct_answers = 0
+
+    @classmethod
+    def load_by_id(cls, db, _id, games_col='games', users_col='users', guestions_col='questions'):
+        instance = cls(db, games_col, users_col, guestions_col)
+        instance._id = _id
+        instance.load_from_db(users_col)
+        return instance
+
+    def load_from_db(self, col):
+        try:
+            result = self.db[col].find({'_id': self._id})[0]
+        except Exception as e:
+            raise e
+        else:
+            for game in result['games']:
+                self.games.append(game)
+            self.questions = result['questions']
+            self.correct_answers = result['correct_answers']
+
+
+class Question:
+    _id = UnchangedTypedPropert(ObjectId)
+    db = UnchangedTypedPropert(Database)
+    category = UnchangedTypedPropert(str, '')
+    question = UnchangedTypedPropert(str, '')
+    correct_answer = UnchangedTypedPropert(str, '')
+    incorrect_answers = UnchangedTypedPropert(list, [])
+    question_code = UnchangedTypedPropert(str, '')
+
+    def __init__(self, db, games_col='games', users_col='users', guestions_col='questions') -> None:
+        self.db = db
+
+    @classmethod
+    def load_by_id(cls, db, _id, games_col='games', users_col='users', guestions_col='questions'):
+        instance = cls(db, games_col, users_col, guestions_col)
+        instance._id = _id
+        instance.load_from_db(guestions_col)
+        return instance
+
+    def load_from_db(self, col):
+        try:
+            result = self.db[col].find({'_id': self._id})[0]
+        except Exception as e:
+            raise e
+        else:
+            self.question = result['question']
+            self.category = result['category']
+            self.correct_answer = result['correct_answer']
+            self.incorrect_answers = result['incorrect_answers']
+            self.question_code = result['question_code']
+
+    def load_new_question(self):
